7035e5252d3e9618939f5b1be4958c726e60d387
LucasGodoyGoncalves <lucasgodoygoncalves2009@gmail.com>
2025-03-14 08:19:09 -0300

    adicionando form

---
+++ ExercíciosPHP/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maior número</title>
+</head>
+<body>
+<fieldset>
+        <legend>Maior número</legend>
+        <form method="post" action="calcular.php">
+            Digite um número:
+            <input type="text" name="numero1" size="25" /> <br/>
+        <br/>
+        Digite outro número:
+        <input type="text" name="numero2" size="25" /> <br/>
+        <input type="submit" name="total" value="calcular" />
+        </form>
+    </fieldset>
+</body>
+</html>
